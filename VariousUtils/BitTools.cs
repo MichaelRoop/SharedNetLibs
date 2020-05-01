18960e84f71408b6c745c2db8c47e9adc681dc74
michael@roop.ca <michael@roop.ca>
2020-05-01 16:39:20 -0300

    Bytes to string,  Bit set check methods

---
+++ VariousUtils/BitTools.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VariousUtils {
+    public static class BitTools {
+
+        public static byte SetBit(byte data, int pos, bool on) {
+            return (on) ? (byte)(data | 1 << pos) : (byte)(data & ~(1 << pos));
+        }
+
+
+
+        /// <summary>Check if the bit numbered pos is set to 1</summary>
+        /// <param name="value">The data member to check</param>
+        /// <param name="pos">The bit position</param>
+        /// <returns>true if bit set to 1</returns>
+        public static bool IsBitSet(this byte value, int pos) {
+            // TODO check limit of bit shift
+            return (value & (1 << pos)) != 0;
+        }
+
+
+        /// <summary>Check if the bit numbered pos is set to 1</summary>
+        /// <param name="value">The data member to check</param>
+        /// <param name="pos">The bit position</param>
+        /// <returns>true if bit set to 1</returns>
+        public static bool IsBitSet(this ushort value, int pos) {
+            // TODO check limit of bit shift
+            return (value & (1 << pos)) != 0;
+        }
+
+
+
+
+    }
+}
--- VariousUtils/StringHelpers.cs
+++ VariousUtils/StringHelpers.cs
@@ -17,6 +17,42 @@ namespace VariousUtils {
         }
 
 
+        public static string ToAsciiString(this byte[] value, int length) {
+            byte[] part = new byte[length];
+            Array.Copy(value, part, length);
+            return Encoding.ASCII.GetString(part);
+        }
+
+
+        /// <summary>Gives ability to display bytes as hex entities in string</summary>
+        /// <param name="data">The data to display</param>
+        /// <returns>String with 0x00,0x12 type format</returns>
+        public static string ToHexByteString(this byte[] data) {
+            StringBuilder sb = new StringBuilder(data.Length + 10);
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append(string.Format("{0}{1:X2}", (i > 0 ? "," : ""), data[i]));
+            }
+            return sb.ToString();
+        }
+
+
+
+        /// <summary>Replace unprintable characters with '*' in string output</summary>
+        /// <param name="data">The incoming bytes</param>
+        /// <returns>A printable string with non-printable characters replaced</returns>
+        public static string ToPrintabletString(byte[] data) {
+            byte[] copy = new byte[data.Length];
+            Array.Copy(data, copy, copy.Length);
+            for (int i = 0; i < data.Length; i++) {
+                if (copy[i] >= 0x00 && copy[i] <= 0x1F) {
+                    copy[i] = 0x2A; // replace with '*'
+                }
+            }
+            return Encoding.ASCII.GetString(copy);
+        }
+
+
+
         public static string CamelCaseToSpaces(this string data) {
             string[] pieces = Regex.Split(data, @"(?<!^)(?=[A-Z])");
             StringBuilder sb = new StringBuilder();
