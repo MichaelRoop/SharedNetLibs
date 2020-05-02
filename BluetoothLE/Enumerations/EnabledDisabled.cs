4663e49121500345287d07929ff025fec6b82f57
michael@roop.ca <michael@roop.ca>
2020-05-02 10:44:43 -0300

    Moved the Bluetooth Classic and BLE for testing

---
+++ BluetoothLE/Enumerations/EnabledDisabled.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BluetoothLE.Net.Enumerations {
+
+    /// <summary>Generic enum for Enabled state</summary>
+    public enum EnabledDisabled {
+        Disabled = 0,
+        Enabled = 1,
+    }
+}
---
+++ BluetoothLE/Enumerations/ReportType.cs
@@ -0,0 +1,9 @@
+﻿
+namespace BluetoothLE.Net.Enumerations {
+    public enum ReportType {
+        Undefined = 0,
+        Input = 1,
+        Output = 2,
+        Feature = 3,
+    }
+}
