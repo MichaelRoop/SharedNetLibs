1874c9c5519bc06ca28c319246b03f22d3692cb5
michael@roop.ca <michael@roop.ca>
2020-09-23 15:39:53 -0300

    Created Core version of state machine demo

---
+++ SpStateMachineDemo.Core.UI/Controls/UC_Output.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpStateMachineDemo.Core.UI.Controls {
+    /// <summary>
+    /// Interaction logic for UC_Output.xaml
+    /// </summary>
+    public partial class UC_Output : UserControl {
+        public enum State {
+            On,
+            Off,
+        }
+
+
+        public string CtrlName {
+            get { return (string)this.GetValue(CtrlNameProperty); }
+            set { this.SetValue(CtrlNameProperty, value); }
+        }
+
+        public bool IsOn { get; private set; }
+
+
+        public UC_Output() {
+            InitializeComponent();
+            // Only funnel the selected scope DataContext to avoid overriding all
+            this.layoutRoot.DataContext = this;
+            this.SetState(State.Off);
+        }
+
+
+        public void SetState(UC_Output.State state) {
+            switch (state) {
+                case UC_Output.State.On:
+                    this.rectBlock.Fill = new SolidColorBrush(Colors.LightGreen);
+                    this.IsOn = true;
+                    break;
+                case UC_Output.State.Off:
+                    this.rectBlock.Fill = new SolidColorBrush(Colors.LightGray);
+                    this.IsOn = false;
+                    break;
+            }
+        }
+
+        #region Dependency binding
+
+        public static readonly DependencyProperty CtrlNameProperty =
+            DependencyProperty.Register(
+                "CtrlName",
+                typeof(string),
+                typeof(UC_Output),
+                new PropertyMetadata(null));
+
+        //https://blog.scottlogic.com/2012/02/06/a-simple-pattern-for-creating-re-useable-usercontrols-in-wpf-silverlight.html
+
+        #endregion
+
+
+    }
+}
---
+++ SpStateMachineDemo.Core.UI/MainWindow.xaml.cs
@@ -0,0 +1,195 @@
+﻿using SpStateMachineDemo.Core.UI.Controls;
+using SpStateMachineDemo.Net.DemoMachine;
+using SpStateMachineDemo.Net.DemoMachine.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpStateMachineDemo.Core.UI {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        DemoMachineWrapper machineWrapper = new DemoMachineWrapper();
+
+        public MainWindow() {
+            InitializeComponent();
+        }
+
+        //private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+
+        //}
+
+        //private void Window_ContentRendered(object sender, EventArgs e) {
+
+        //}
+
+        #region Windows events
+
+        private void Window_ContentRendered(object sender, EventArgs e) {
+            this.machineWrapper.StateMachineStateChange += this.MachineWrapper_StateMachineStateChange;
+            this.machineWrapper.OutputStateChange += this.Outputs_StateChange;
+            this.machineWrapper.InputStateChange += this.Inputs_StateChange;
+            this.machineWrapper.Init();
+
+            LogUtils.Net.Log.Warning(0, "In the content rendered");
+
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            // We could intercept this until done
+            this.machineWrapper.Teardown();
+            this.machineWrapper.OutputStateChange -= this.Outputs_StateChange;
+            this.machineWrapper.InputStateChange -= this.Inputs_StateChange;
+            this.machineWrapper.StateMachineStateChange -= this.MachineWrapper_StateMachineStateChange;
+        }
+
+        #endregion
+
+        #region Fake a hardware output change to exercise state machine
+
+        private void btnOxygenOut_Click(object sender, RoutedEventArgs e) {
+            this.machineWrapper.ToggleIO(OutputId.GasOxygen);
+        }
+
+        private void btnNitrogenOut_Click(object sender, RoutedEventArgs e) {
+            this.machineWrapper.ToggleIO(OutputId.GasNitrogen);
+        }
+
+        private void btnHeatOut_Click(object sender, RoutedEventArgs e) {
+            this.machineWrapper.ToggleIO(OutputId.Heater);
+        }
+
+        #endregion
+
+        #region Fake a hardware input change to exercise state machine
+
+        private void btnOxygenIn_Click(object sender, RoutedEventArgs e) {
+            this.machineWrapper.ToggleIO(InputId.GasOxygen);
+        }
+
+        private void btnNitrogenIn_Click(object sender, RoutedEventArgs e) {
+            this.machineWrapper.ToggleIO(InputId.GasNitrogen);
+        }
+
+        private void btnHeatIn_Click(object sender, RoutedEventArgs e) {
+            this.machineWrapper.ToggleIO(InputId.Heater);
+        }
+
+        #endregion
+
+        #region Button events
+
+        private void btnAbort_Click(object sender, RoutedEventArgs e) {
+        }
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e) {
+        }
+
+
+        private void btnExit_Click(object sender, RoutedEventArgs e) {
+            this.Close();
+        }
+
+        #endregion
+
+        #region Private
+
+        private void MachineWrapper_StateMachineStateChange(object sender, EventArgs e) {
+            this.Dispatcher.Invoke(() => {
+                this.txtState.Text = ((StateChangeArgs)e).State;
+            });
+        }
+
+
+        private void Inputs_StateChange(object sender, EventArgs e) {
+            this.Dispatcher.Invoke(() => {
+                InputChangeArgs args = (InputChangeArgs)e;
+                this.GetCtrl(args.Id).Set(args.State);
+            });
+        }
+
+
+        /// <summary>Event handler from ouputs</summary>
+        /// <param name="sender">The object sending the event (DemoOutput)</param>
+        /// <param name="e">Event args (OutputChangeArgs)</param>
+        private void Outputs_StateChange(object sender, EventArgs e) {
+            this.Dispatcher.Invoke(() => {
+                OutputChangeArgs args = (OutputChangeArgs)e;
+                this.GetCtrl(args.Id).Set(args.State);
+            });
+        }
+
+
+        /// <summary>Get the IO visual for an IO output</summary>
+        /// <param name="id">The state machine output id</param>
+        /// <returns>The corresponding visual object</returns>
+        UC_Output GetCtrl(OutputId id) {
+            switch (id) {
+                case OutputId.GasNitrogen:
+                    return this.gasNitrogenOut;
+                case OutputId.GasOxygen:
+                    return this.gasOxygenOut;
+                case OutputId.Heater:
+                    return this.heaterOut;
+                default:
+                    return this.gasNitrogenOut;
+            }
+        }
+
+
+        /// <summary>Get the IO visual for an IO input</summary>
+        /// <param name="id">The state machine input id</param>
+        /// <returns>The corresponding visual object</returns>
+        UC_Output GetCtrl(InputId id) {
+            switch (id) {
+                case InputId.GasNitrogen:
+                    return this.gasNitrogenIn;
+                case InputId.GasOxygen:
+                    return this.gasOxygenIn;
+                case InputId.Heater:
+                    return this.heaterIn;
+                default:
+                    return this.gasNitrogenIn;
+            }
+        }
+
+        #endregion
+
+
+
+    }
+
+
+    #region Extensions
+
+    /// <summary>Extensions to join the IO objects with the UI controls</summary>
+    public static class IOCtrlExtensions {
+
+        /// <summary>Set the visual state of the IO control from the state machine state</summary>
+        /// <param name="ctrl">The visual control</param>
+        /// <param name="state">The state to change</param>
+        public static void Set(this UC_Output ctrl, IOState state) {
+            ctrl.SetState(state == IOState.On ? UC_Output.State.On : UC_Output.State.Off);
+        }
+    }
+
+    #endregion
+
+
+
+}
