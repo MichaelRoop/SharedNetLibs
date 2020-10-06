75fd29f424364ca047679e770f220cede5d7f190
michael@roop.ca <michael@roop.ca>
2020-10-06 19:35:48 -0300

    documented pairing info object

--- BluetoothCommon.Net/BT_PairInfoRequest.cs
+++ BluetoothCommon.Net/BT_PairInfoRequest.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace BluetoothCommon.Net {
 
-namespace BluetoothCommon.Net {
-    
+    /// <summary>Information to pass up and down to execute pairing</summary>
     public class BT_PairInfoRequest {
 
+        /// <summary>User friendly name</summary>
         public string DeviceName { get; set; } = "NA";
 
+        /// <summary>BT sets true if a PIN is required</summary>
         public bool PinRequested { get; set; } = false;
 
+        /// <summary>The PIN entered by the user</summary>
         public string Pin { get; set; } = "";
 
+        /// <summary>User sets true if pairing is requested</summary>
         public bool Response { get; set; } = false;
 
     }
