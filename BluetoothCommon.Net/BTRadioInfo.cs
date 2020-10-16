ea223fa93e0ad1dca2c42bd62bd49b8740881e1c
michael@roop.ca <michael@roop.ca>
2020-10-16 00:14:32 -0300

    IsInitialized field in Radio object

--- BluetoothCommon.Net/BTRadioInfo.cs
+++ BluetoothCommon.Net/BTRadioInfo.cs
@@ -5,6 +5,7 @@ namespace BluetoothCommon.Net {
 
     /// <summary>To get information about the BT Classic radio</summary>
     public class BTRadioInfo {
+        public bool IsInitialized { get; set; } = false;
         public string Manufacturer { get; set; } = "N/A";
         public string LinkManagerProtocol { get; set; } = "N/A";
         public List<string> Features { get; set; } = new List<string>();
