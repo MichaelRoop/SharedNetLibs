4e7f64596125b9dd21870bcfd164d98f0e2f3b4b
michael@roop.ca <michael@roop.ca>
2020-10-18 20:13:47 -0300

    Msg pump interface and connect data model

---
+++ CommunicationStack.Net/DataModels/MsgPumpConnectResults.cs
@@ -0,0 +1,17 @@
+﻿namespace CommunicationStack.Net.DataModels {
+
+    /// <summary>Data model to hold results of socket connection</summary>
+    public class MsgPumpConnectResults {
+
+        /// <summary>Success status</summary>
+        public bool IsSuccessful { get; set; } = false;
+        // TODO - add specific code
+
+
+        public MsgPumpConnectResults() { }
+        public MsgPumpConnectResults(bool success) {
+            this.IsSuccessful = success;
+        }
+
+    }
+}
---
+++ CommunicationStack.Net/interfaces/IMsgPump.cs
@@ -0,0 +1,49 @@
+﻿using CommunicationStack.Net.DataModels;
+using LogUtils.Net;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CommunicationStack.Net.interfaces {
+
+    /// <summary>Interface for a communications message pump</summary>
+    /// <typeparam name="TConnectParams">The connect parameters object type</typeparam>
+    public interface IMsgPump<TConnectParams> where TConnectParams : class {
+
+        #region Properties
+
+        /// <summary>Connected status</summary>
+        bool Connected { get; }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>Raised on completion of connection attempt</summary>
+        event EventHandler<MsgPumpConnectResults> ConnectResultEvent;
+
+        /// <summary>Raised on each message received</summary>
+        event EventHandler<byte[]> MsgReceivedEvent;
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>Do asynchronous connect to socket and raise ConnectResultEvent</summary>
+        /// <param name="paramsObj">Data model holding the co</param>
+        void ConnectAsync(TConnectParams paramsObj);
+
+        /// <summary>Stop reading or sending if connected, then disconnect</summary>
+        void Disconnect();
+
+        /// <summary>Send out a message asynchronously</summary>
+        /// <param name="msg">The message to send</param>
+        void WriteAsync(byte[] msg);
+
+        #endregion
+
+    }
+
+
+
+}
