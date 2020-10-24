604c3040af6f39c9995df818b1a5e7f24add648b
michael@roop.ca <michael@roop.ca>
2020-10-24 12:46:50 -0300

    Properties display data model

---
+++ BluetoothLE/DataModels/BLE_PropertyDataModelDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluetoothLE.Net.DataModels {
+    
+    public class BLE_PropertyDataModelDisplay {
+
+        public string Key { get; set; } = string.Empty;
+        public string Value { get; set; } = string.Empty;
+        public string DataType { get; set; } = string.Empty;
+
+        public BLE_PropertyDataModelDisplay(BLE_PropertyDataModel data) {
+            this.Key = data.Key;
+            if (data.DataType == PropertyDataType.TypeString) {
+                this.Value = string.Format("\"{0}\"", data.Value.ToString());
+            }
+            else {
+                this.Value = data.Value.ToString();
+            }
+            this.DataType = data.DataType.ToFriendlyString();
+        }
+    }
+
+
+
+
+
+}
