63c2520a567107b6b0399f420ad7d0a9cf1692b1
michael@roop.ca <michael@roop.ca>
2020-10-25 00:22:10 -0300

    Start on the USB communications

---
+++ SerialCommon.Net/DataModels/SerialInfo.cs
@@ -0,0 +1,19 @@
+﻿using Common.Net.Network;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerialCommon.Net.DataModels {
+
+    public class SerialInfo {
+        public string Name { get; set; } = string.Empty;
+        public bool IsDefault { get; set; } = false;
+        public bool IsEnabled { get; set; } = false;
+
+        // Not sure if the same
+        public Dictionary<string, NetPropertyDataModel> Properties { get; set; } = new Dictionary<string, NetPropertyDataModel>();
+        // Device information kind
+
+}
+
+}
---
+++ SerialCommon.Net/interfaces/ISerialInterface.cs
@@ -0,0 +1,56 @@
+﻿using CommunicationStack.Net.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SerialCommon.Net.interfaces {
+
+    public interface ISerialInterface : ICommStackChannel {
+
+
+        #region Events
+
+        ///// <summary>Raised with list of adapters found in discovery</summary>
+        //event EventHandler<List<WifiAdapterInfo>> DiscoveredAdapters;
+
+        ///// <summary>Raised with list of networks found in discovery</summary>
+        //event EventHandler<List<WifiNetworkInfo>> DiscoveredNetworks;
+
+        ///// <summary>For various errors encountered in asynchronous operations</summary>
+        //event EventHandler<WifiError> OnError;
+
+        ///// <summary>Async Connection completed</summary>
+        //event EventHandler<MsgPumpConnectResults> OnWifiConnectionAttemptCompleted;
+
+        ///// <summary>Raised if there is no password, host name or service name in the connection data model</summary>
+        //event EventHandler<WifiCredentials> CredentialsRequestedEvent;
+
+        #endregion
+
+        #region Properties
+
+        bool Connected { get; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Asynchronous discovery of WIFI adapters with DiscoveredAdapters event 
+        /// raised on completion
+        /// </summary>
+        void DiscoverSerialDevicesAsync();
+
+
+        /// <summary>Connect to a WIFI network</summary>
+        /// <param name="dataModel">The information for connection</param>
+        //void ConnectAsync(WifiNetworkInfo dataModel);
+
+
+        void Disconnect();
+
+        #endregion
+
+
+    }
+}
