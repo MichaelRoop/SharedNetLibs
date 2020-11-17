4b4bd8e7895adfd74ba3f2237fa7fcedb59f4835
michael@roop.ca <michael@roop.ca>
2020-11-17 12:21:34 -0400

    IconFactory do nothing implementation

---
+++ IconFactory/IconFactoryDoNothingImplementation.cs
@@ -0,0 +1,17 @@
+﻿using IconFactory.Net.data;
+using IconFactory.Net.interfaces;
+
+namespace IconFactory.Net {
+
+    public class IconFactoryDoNothingImplementation : IIconFactory {
+
+        public IconDataModel GetIcon(UIIcon code) {
+            return new IconDataModel() {
+                Code = code,
+                IconSource = string.Empty,
+                Padding = string.Empty,
+            };
+        }
+
+    }
+}
