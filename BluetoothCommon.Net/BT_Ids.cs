bb56c139b6d553deebc757254693d4b132c57966
michael@roop.ca <michael@roop.ca>
2020-11-19 13:57:26 -0400

    Id class for BT Guids

---
+++ BluetoothCommon.Net/BT_Ids.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluetoothCommon.Net {
+
+    /// <summary>Full Guid identifiers</summary>
+    public static class BT_Ids {
+
+        public const string SerialServiceGuid = "00001101-0000-1000-8000-00805f9b34fb";
+
+    }
+}
