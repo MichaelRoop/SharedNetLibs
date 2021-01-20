f88cd33613086100b5fee42701315b0d2fbb3554
michael@roop.ca <michael@roop.ca>
2021-01-20 17:35:55 -0400

    Fixed Characteristic appearance mask

--- BluetoothLE/Parsers/Characteristics/CharParser_Appearance.cs
+++ BluetoothLE/Parsers/Characteristics/CharParser_Appearance.cs
@@ -15,10 +15,10 @@ namespace BluetoothLE.Net.Parsers.Characteristics {
         protected override bool DoParse(byte[] data) {
             //https://specificationrefs.bluetooth.com/assigned-values/Appearance%20Values.pdf
             //  6 bits = sub category - bits 0-5  - Mask 0000 0000 0011 1111 (63)
-            // 10 bits = category     - bits 6-15 - Mask 1111 1111 1100 0000 (65,534)
-            if (this.CopyToRawData(data, data.Length)) {
+            // 10 bits = category     - bits 6-15 - Mask 1111 1111 1100 0000 (65,472)
+            if (this.CopyToRawData(data, 2)) {
                 uint raw = (uint)BitConverter.ToUInt16(this.RawData);
-                uint catMask = 65534;
+                uint catMask = 65472;
                 uint subMask = 63;
                 StringBuilder sb = new StringBuilder();
                 sb.Append((raw & catMask)).Append(",").Append((raw & subMask));
