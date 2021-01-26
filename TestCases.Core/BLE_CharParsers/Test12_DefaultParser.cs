151689c272132c96c6d94473b4159c1b07d97e15
michael@roop.ca <michael@roop.ca>
2021-01-26 13:46:04 -0400

    BLE PresentationFormat with Default Char Test cases

--- TestCases.Core/BLE_CharParsers/Test12_DefaultParser.cs
+++ TestCases.Core/BLE_CharParsers/Test12_DefaultParser.cs
@@ -35,40 +35,99 @@ namespace TestCases.Core.BLE_CharParsers {
 
         #region Data
 
-        ClassLog log = new ClassLog("DescFormatParserTest");
-
+        private ClassLog log = new ClassLog("DescFormatParserTest");
+        private ICharParser defaultCharParser = new CharParser_Default();
 
         #endregion
 
         #region Test helpers
 
         [Test]
-        public void WithDefaultChar_FormatString() {
+        public void UTF8String() {
             TestHelpersNet.CatchUnexpected(() => {
                 IDescParser parser = new DescParser_PresentationFormat();
-                byte[] data = this.GetBlock(DataFormatEnum.UTF8_String, UnitsOfMeasurement.Unitless);
-                string result = parser.Parse(data);
-                this.log.Info("WithDefaultChar_FormatString",
-                    () => string.Format("Format Desc Display:{0}", result));
-
-                List<IDescParser> descriptors = new List<IDescParser>();
-                descriptors.Add(parser);
-
-                ICharParser charParser = new CharParser_Default();
-                charParser.SetDescriptorParsers(descriptors);
-
+                this.SetFormatDesc(this.GetBlock(DataFormatEnum.UTF8_String, UnitsOfMeasurement.Unitless));
                 string str = "This is a sample string for formating";
-                result = charParser.Parse(Encoding.UTF8.GetBytes(str));
-                this.log.Info("WithDefaultChar_FormatString",
-                    () => string.Format("Default Char Display '{0}'", result));
+                string result = this.defaultCharParser.Parse(Encoding.UTF8.GetBytes(str));
+                this.log.Info("UTF16String", () => string.Format("UTF8String '{0}'", result));
                 Assert.AreEqual(str, result);
+            });
+        }
 
+        [Test]
+        public void UTF16String() {
+            TestHelpersNet.CatchUnexpected(() => {
+                IDescParser parser = new DescParser_PresentationFormat();
+                this.SetFormatDesc(this.GetBlock(DataFormatEnum.UTF16_String, UnitsOfMeasurement.Unitless));
+                string str = "This is a sample string for formating";
+                string result = this.defaultCharParser.Parse(Encoding.Unicode.GetBytes(str));
+                this.log.Info("UTF16String", () => string.Format("UTF16String '{0}'", result));
+                Assert.AreEqual(str, result);
+            });
+        }
+
+        [Test]
+        public void Uint2Bit() {
+            this.TestByteBits(this.GetByteArray(3), "3", DataFormatEnum.Unsigned_2bit_integer);
+        }
+
+
+        [Test]
+        public void Uint2BitBitsMaskedOut() {
+            this.TestByteBits(this.GetByteArray(15), "3", DataFormatEnum.Unsigned_2bit_integer);
+        }
+
+
+        [Test]
+        public void Uint4Bit() {
+            this.TestByteBits(this.GetByteArray(14), "14", DataFormatEnum.unsigned_4bit_integer);
+        }
+
+        [Test]
+        public void Uint4BitBitsMaskedOut() {
+            this.TestByteBits(this.GetByteArray(255), "15", DataFormatEnum.unsigned_4bit_integer);
+        }
+
+        [Test]
+        public void Uint24BitNotEnoughBytes() {
+            this.TestByteBits(this.GetByteArray(100), "ERR", DataFormatEnum.unsigned_24bit_integer);
+        }
+
+
+
+        // Not yet implemented
+        //[Test]
+        //public void Int12Bit() {
+        //    this.TestByteBits(this.GetSByteArray(-53), "-53", DataFormatEnum.signed_12bit_integer);
+        //}
+
+
+
+        public void TestByteBits(byte[] data, string expected, DataFormatEnum format) {
+            TestHelpersNet.CatchUnexpected(() => {
+                IDescParser parser = new DescParser_PresentationFormat();
+                this.SetFormatDesc(this.GetBlock(format, UnitsOfMeasurement.Unitless));
+                string result = this.defaultCharParser.Parse(data);
+                this.log.Info("TestByteBits", () => string.Format("Value:'{0}'", result));
+                Assert.AreEqual(expected, result);
             });
         }
 
 
 
         #endregion
+
+
+        private void SetFormatDesc(byte[] data) {
+            IDescParser formatParser = new DescParser_PresentationFormat();
+            string result = formatParser.Parse(data);
+            this.log.Info("SetFormatDesc", () => string.Format("Format Desc Display:{0}", result));
+            List<IDescParser> descriptors = new List<IDescParser>();
+            descriptors.Add(formatParser);
+            this.defaultCharParser.SetDescriptorParsers(descriptors);
+        }
+
+
 
         #region GetBlock
 
@@ -107,6 +166,22 @@ namespace TestCases.Core.BLE_CharParsers {
             description.WriteToBuffer(data, ref pos);
             return data;
         }
+
+
+        byte[] GetByteArray(byte value) {
+            int pos = 0;
+            byte[] data = new byte[1];
+            value.WriteToBuffer(data, ref pos);
+            return data;
+        }
+
+        byte[] GetSByteArray(sbyte value) {
+            int pos = 0;
+            byte[] data = new byte[1];
+            value.WriteToBuffer(data, ref pos);
+            return data;
+        }
+
 
         #endregion
 
