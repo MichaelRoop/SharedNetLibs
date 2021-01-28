bd9f33263077bb56fe4c860f4a83636771220ac3
michael@roop.ca <michael@roop.ca>
2021-01-28 13:37:34 -0400

    BLE notes and dummy descriptor parsers

---
+++ BluetoothLE/Parsers/Descriptor/DescParser_CompleteBR_EDRTransportBlockData.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluetoothLE.Net.Parsers.Descriptor {
+
+    // TODO - Implements
+    class DescParser_CompleteBR_EDRTransportBlockData {
+    }
+}
---
+++ BluetoothLE/Parsers/Descriptor/DescParser_ExternalReportReference.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluetoothLE.Net.Parsers.Descriptor {
+
+    // TODO - implement
+    class DescParser_ExternalReportReference {
+    }
+}
