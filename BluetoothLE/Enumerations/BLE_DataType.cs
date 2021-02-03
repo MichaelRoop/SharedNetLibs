01d052249ec7aaeeff1f4b102ea3f0a49c2c73e1
michael@roop.ca <michael@roop.ca>
2021-02-03 13:51:04 -0400

    BLE new enum for extended data types

---
+++ BluetoothLE/Enumerations/BLE_DataType.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BluetoothLE.Net.Enumerations {
+    public enum BLE_DataType : int {
+
+        // First entries are taken from the descriptor data type. Do not change
+        Reserved = DataFormatEnum.Reserved,
+        Bool = DataFormatEnum.Boolean,
+        UInt_2bit = DataFormatEnum.UInt_2bit,
+        UInt_4bit = DataFormatEnum.UInt_4bit,
+        UInt_8bit = DataFormatEnum.UInt_8bit,
+        UInt_12bit = DataFormatEnum.UInt_12bit,
+        UInt_16bit = DataFormatEnum.UInt_16bit,
+        UInt_24bit = DataFormatEnum.UInt_24bit,
+        UInt_32bit = DataFormatEnum.UInt_32bit,
+        UInt_48bit = DataFormatEnum.UInt_48bit,
+        UInt_64bit = DataFormatEnum.UInt_64bit,
+        UInt_128bit = DataFormatEnum.UInt_128bit,
+        Int_8bit = DataFormatEnum.Int_8bit,
+        Int_12bit = DataFormatEnum.Int_12bit,
+        Int_16bit = DataFormatEnum.Int_16bit,
+        Int_24bit = DataFormatEnum.Int_24bit,
+        Int_32bit = DataFormatEnum.Int_32bit,
+        Int_48bit = DataFormatEnum.Int_48bit,
+        Int_64bit = DataFormatEnum.Int_64bit,
+        Int_128bit = DataFormatEnum.Int_128bit,
+        // 754 is the currently supported in MS
+        IEEE_754_32bit_floating_point = DataFormatEnum.IEEE_754_32bit_floating_point,
+        IEEE_754_64bit_floating_point = DataFormatEnum.IEEE_754_64bit_floating_point,
+        IEEE_11073_16bit_SFLOAT = DataFormatEnum.IEEE_11073_16bit_SFLOAT,
+        IEEE_11073_32bit_FLOAT = DataFormatEnum.IEEE_11073_32bit_FLOAT,
+        IEEE_20601_format = DataFormatEnum.IEEE_20601_format,
+        UTF8_String = DataFormatEnum.UTF8_String,
+        UTF16_String = DataFormatEnum.UTF16_String,
+        OpaqueStructure = DataFormatEnum.OpaqueStructure,
+
+        // End of Descriptor format type indicators
+        // Last one was 0x1B - their max is 0xFF because it is a byte
+        // Start the higher level series at 0x100 (256)
+        // Can add different structure types
+
+
+
+        // End of Descriptor Format data type
+
+
+    }
+}
--- BluetoothLE/Enumerations/DataFormatEnum.cs
+++ BluetoothLE/Enumerations/DataFormatEnum.cs
@@ -2,37 +2,42 @@
 namespace BluetoothLE.Net.Enumerations {
 
     /// <summary>Used in the format presenter descriptor</summary>
+    /// <remarks>
+    /// https://www.bluetooth.com/specifications/assigned-numbers/format-types/
+    /// </remarks>
     public enum DataFormatEnum : byte {
         // Do * NOT * change order. Lines up with spec
-        Reserved = 0,
-        Boolean = 1,
-        UInt_2bit = 2,
-        UInt_4bit = 3,
-        UInt_8bit = 4,
-        UInt_12bit = 5,
-        UInt_16bit = 6,
-        UInt_24bit = 7,
-        UInt_32bit = 8,
-        UInt_48bit = 9,
-        UInt_64bit = 10,
-        UInt_128bit = 11,
-        Int_8bit = 12,
-        Int_12bit = 13,
-        Int_16bit = 14,
-        Int_24bit = 15,
-        Int_32bit = 16,
-        Int_48bit = 17,
-        Int_64bit = 18,
-        Int_128bit = 19,
+        Reserved = 0x00,  // No exponent
+        Boolean = 0x01,   // No exponent
+        UInt_2bit = 0x02, // No exponent
+        UInt_4bit = 0x03, // No exponent
+        UInt_8bit = 0x04, // No exponent
+        UInt_12bit = 0x05,
+        UInt_16bit = 0x06,
+        UInt_24bit = 0x07,
+        UInt_32bit = 0x08, // Used for IPv4 addresses
+        UInt_48bit = 0x09, // Used for BT BD_ADDR address
+        UInt_64bit = 0x0A,
+        UInt_128bit = 0x0B,
+        Int_8bit = 0x0C,
+        Int_12bit = 0x0D,
+        Int_16bit = 0x0E,
+        Int_24bit = 0x0F,
+        Int_32bit = 0x10,
+        Int_48bit = 0x11,
+        Int_64bit = 0x12,
+        Int_128bit = 0x13, // Used for IPv6 addresses
         // 754 is the currently supported in MS
-        IEEE_754_32bit_floating_point = 20,
-        IEEE_754_64bit_floating_point = 21,
-        IEEE_11073_16bit_SFLOAT = 22,
-        IEEE_11073_32bit_FLOAT = 23,
-        IEEE_20601_format = 24,
-        UTF8_String = 25,
-        UTF16_String = 26,
-        OpaqueStructure = 27,
+        IEEE_754_32bit_floating_point = 0x14,// No exponent
+        IEEE_754_64bit_floating_point = 0x15,// No exponent
+        IEEE_11073_16bit_SFLOAT = 0x16,     // No exponent
+        IEEE_11073_32bit_FLOAT = 0x17,      // No exponent
+        IEEE_20601_format = 0x18,           // No exponent - dunit. 2 Uint16 values concatenated
+        UTF8_String = 0x19,                 // No exponent
+        UTF16_String = 0x1A,                // No exponent
+        OpaqueStructure = 0x1B,             // No exponent
+        // BLE 0x1C to 0xFF reserved
+
 
         // My own enum to 
         Unhandled = 255,
