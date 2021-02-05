dc263def9b595c334ce92c72a20ce860e952fe4b
michael@roop.ca <michael@roop.ca>
2021-02-05 16:47:19 -0400

    New test case support project

---
+++ TestCaseSupport.Core/TestCaseBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace TestCaseSupport.Core {
+
+    public class TestCaseBase {
+
+        #region Data
+
+        protected HelperLogReader logReader = new HelperLogReader();
+
+        #endregion
+
+        public virtual void OneTimeSetup() {
+            try {
+                this.logReader.StartLogging();
+            }
+            catch (Exception e) {
+                Debug.WriteLine("Start logging exception:{0}", e.Message);
+            }
+        }
+
+        public virtual void OneTimeTeardown() {
+            System.Threading.Thread.Sleep(1000);
+//            this.logReader.StopLogging();
+//            this.logReader.Clear();
+        }
+
+
+
+    }
+}
